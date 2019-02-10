37d7318d7d29cb9bb2085991a8f614770f0561a8
shbatm <support@shbatm.com>
2019-02-10 00:04:09 -0600

    Correct malformed Climate commands
    Bump Version to 1.1.2
    
    
    Additional fixes to Climate Functions
    
    
    Expand Event Results to include value, precision, and UOM
    
    Expanded the Event Results to include more than just the control command.
    
    For climate nodes, not all properties are stored in the actual node definition returned by the ISY. CLIHCS, CLIFS, UOM for example, are only seen as events, and the values are required to do anything useful with them.
    
    A Class has been implemented to capture the other information, but still report back only a string so this is not a breaking change.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyISY',
-    version='1.1.1',
+    version='1.1.2',
     license='Apache License 2.0',
     url='http://automic.us/projects/pyisy',
-    download_url='https://github.com/automicus/pyisy/tarball/1.1.1',
+    download_url='https://github.com/automicus/pyisy/tarball/1.1.2',
     author='Ryan Kraus',
     author_email='automicus@gmail.com',
     description='Python module to talk to ISY994 from UDI.',
