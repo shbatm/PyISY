a2e538892558d6f7109ce4ebb3dbe2f2238b0725
shbatm <support@shbatm.com>
2019-07-15 19:55:52 -0400

    Version bump to 1.1.12

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='PyISY_beta',
-    version='1.1.11',
+    version='1.1.12',
     license='Apache License 2.0',
     url='https://github.com/shbatm/PyISY/tree/PyISY_beta',
     long_description=README,
